fa6799426fa791e44ab5e59c64fd586c9a02e608
Thao Pham <thaopham2216@gmail.com>
2022-04-07 12:09:05 -0400

    Mongoose Part 3

--- 5-NodeJS-Express-MongoDB/node-mongoose/models/campsite.js
+++ 5-NodeJS-Express-MongoDB/node-mongoose/models/campsite.js
@@ -1,5 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        require: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
 
 const campsiteSchema = new Schema({
     name: {
@@ -10,7 +28,8 @@ const campsiteSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
 }, {
     timestamps: true
 });
