cac0caaa7c227fc6ab74eea5ef8de9a5d9546417
Thao Pham <thaopham2216@gmail.com>
2022-04-06 23:44:17 -0400

    Node and MongoDB Part 2

--- 5-NodeJS-Express-MongoDB/node-mongo/index.js
+++ 5-NodeJS-Express-MongoDB/node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert').strict;
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
@@ -19,22 +20,34 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
         assert.strictEqual(err, null);
         console.log('Dropped Collection', result);
 
-        //recreate new campsites collection, inserted new document Breadcrumb to the campsite collection 
-        const collection = db.collection('campsites');
+        //        collection.insertOne({ name: "Breadcrumb Trail Campground", description: "Test" },
+        dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test"},
+            'campsites', result => {
+            console.log('Insert Document:', result.ops);
 
-        collection.insertOne({ name: "Breadcrumb Trail Campground", description: "Test" },
-            (err, result) => {
-                assert.strictEqual(err, null);
-                console.log('Insert Document:', result.ops);
+            dboper.findDocuments(db, 'campsites', docs => {
+                console.log('Found Documents:', docs);
 
-                //collection.find help console.log all the document from the campsite collection 
-                collection.find().toArray((err, docs) => {
-                    assert.strictEqual(err, null);
-                    console.log('Found Documents:', docs);
-                    // close the client 
-                    client.close();
-                });
+                dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
+                    { description: "Updated Test Description" }, 'campsites',
+                    result => {
+                        console.log('Updated Document Count:', result.result.nModified);
+
+                        dboper.findDocuments(db, 'campsites', docs => {
+                            console.log('Found Documents:', docs);
+                            
+                            dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
+                                'campsites', result => {
+                                    console.log('Deleted Document Count:', result.deletedCount);
+
+                                    client.close();
+                                }
+                            );
+                        });
+                    }
+                );
             });
+        });
     });
 });
 //handle error by the (err, null) call back function 
---
+++ 5-NodeJS-Express-MongoDB/node-mongo/operations.js
@@ -0,0 +1,35 @@
+//interaction between node and mongodb server  part 2 4 methods insert, find, remove and update 
+const assert = require('assert').strict;
+
+//4 parameter(db, document, collection, callback ), coll short for collection 
+exports.insertDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+    coll.insertOne(document, (err, result) => {
+        assert.strictEqual(err, null);
+        callback(result);
+    });
+};
+
+exports.findDocuments = (db, collection, callback) => {
+    const coll = db.collection(collection);
+    coll.find().toArray((err, docs) => {
+        assert.strictEqual(err, null);
+        callback(docs);
+    });
+};
+
+exports.removeDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+    coll.deleteOne(document, (err, result) => {
+        assert.strictEqual(err, null);
+        callback(result);
+    });
+};
+
+exports.updateDocument = (db, document, update, collection, callback) => {
+    const coll = db.collection(collection);
+    coll.updateOne(document, { $set: update }, null, (err, result) => {
+        assert.strictEqual(err, null);
+        callback(result);
+    });
+};
